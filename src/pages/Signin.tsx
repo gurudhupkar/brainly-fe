3cb35adb8ef3b823496115b833e7544db9e82f09
Guru Dhupkar <guruprasaddhupkar@gmail.com>
2025-01-20 00:02:35 +0530

    Sigin

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -23,6 +23,7 @@ export function Signin() {
         })
         const jwt= response.data.token;
         localStorage.setItem("token", jwt);
+        navigate("/dashboard")
        
         if (response.data.message){
         alert(response.data.message)
