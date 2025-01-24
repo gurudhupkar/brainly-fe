56b089a8dd33303a5a21b2228305f88087a75e3c
Guru Dhupkar <guruprasaddhupkar@gmail.com>
2025-01-25 00:40:17 +0530

    ShareLink Functionality Done

--- src/componets/Dashboard1.tsx
+++ src/componets/Dashboard1.tsx
@@ -1,78 +1,85 @@
+import { Button } from "../componets/Button";
+import { Shareicon } from "../icons/shareicon";
+import { Plusicon } from "../icons/plusicon";
+import { Card } from "../componets/card";
+import { Createcontentmodel } from "../componets/createcontent";
+import { useEffect, useState } from "react";
+import { Sidebar } from "../componets/sidebar";
+import { Logout } from "../componets/logout";
+import axios from "axios";
+import { Backend_URl } from "../config";
 
-import { Button } from "../componets/Button"
-import { Shareicon} from "../icons/shareicon"
-import { Plusicon } from "../icons/plusicon"
-import {Card} from "../componets/card"
-import { Createcontentmodel } from "../componets/createcontent"
-import { useEffect, useState } from "react"
-import { Sidebar } from "../componets/sidebar"
-import { Usecontent } from "../hooks/usecontent"
-import { Logout } from "../componets/logout"
-import axios from "axios"
-import { Backend_URl } from "../config"
+export function DashBoard1() {
+  const [modelopen, setclosemodel] = useState(false);
+  const [content, setContent] = useState([]);
+ 
+  const [error, setError] = useState();
 
-export function DashBoard1()
-{
-
-  const [modelopen , setclosemodel] = useState(false)
-  const [content , Setcontent] = useState(false)
-
-//   const {content ,refresh} = Usecontent();
-
-  useEffect(()=>{
-    const url = (window.location.pathname.split("/")[2])
-   const response= axios.get(`${Backend_URl}/api/v1/user/share/${url}`,{
+  useEffect(() => {
     
-    
+    const fetchdata = async ()=>{
+      try{
+        const url = window.location.pathname.split("/")[2];
+        const response = await axios.get (`${Backend_URl}/api/v1/user/share/${url}`);
         
-
-    })
-   
-    
-    console.log(url)
-
-  },[])
-
-
-
-  return <div >
-    <Sidebar />
-    <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
-    <Createcontentmodel open={modelopen} onClose={()=>{
-      setclosemodel(false)
-    }} />
-    <div className="flex justify-end gap-4">
+        console.log(response.data);
+        if(response.data.success)
+        {
+          setContent(response.data.content)
+        }
+        else {
+          setError(response.data.message);
+        }
 
 
-   <Button onClick={()=>{
-    setclosemodel(true)
-   }} variant="primary" text="Add Content"starticon={<Plusicon/>}></Button>
-   <Button onClick={async()=>{
-   
-   
-     
-   }} variant="secondary" text="Share-brain" starticon={<Shareicon/>}></Button>
-   <Logout />
-    </div>
-   
+      }
+      catch (err){
+        console.error(error);
+        setError(error);
+
+      }
+    }
+   fetchdata();
    
 
-   <div className="flex gap-4 flex-wrap">
-   
 
-   {/* {content.map(({type,title,link})=><Card 
-    type={type}
-    title={title}
-    link ={link} 
-    />
+  }, []);
+
+  return  <div >
+  <Sidebar />
+  <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
+  <Createcontentmodel open={modelopen} onClose={()=>{
+    setclosemodel(false)
+  }} />
+  <div className="flex justify-end gap-4">
+
+
+ <Button onClick={()=>{
+  setclosemodel(true)
+ }} variant="primary" text="Add Content"starticon={<Plusicon/>}></Button>
+ <Button variant="secondary" text="Share-brain" starticon={<Shareicon/>}></Button>
+ <Logout />
+  </div>
+ 
+ 
+
+ <div className="flex gap-4 flex-wrap">
+ 
+
+ {content.map(({type,title,link})=><Card 
+  type={type}
+  title={title}
+  link ={link} 
+  />
+ 
+
+)}
+
+ </div>
+ 
+ </div>
+  
+</div>
    
   
-  )} */}
-
-   </div>
-   
-   </div>
-    
-  </div>
 }
-
